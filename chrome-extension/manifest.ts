a0ef6b6b38e5997cd586de7c39cab9ba7f0c385d
Giannis Riganas <giannis.riganas@allwyn-lotterysolutions.com>
2025-09-08 12:54:37 +0300

    Remove unused features

--- chrome-extension/manifest.ts
+++ chrome-extension/manifest.ts
@@ -31,8 +31,7 @@ const manifest = {
   version: packageJson.version,
   description: '__MSG_extensionDescription__',
   host_permissions: ['<all_urls>'],
-  permissions: ['storage', 'scripting', 'tabs', 'notifications', 'sidePanel'],
-  options_page: 'options/index.html',
+  permissions: ['storage', 'scripting', 'tabs', 'notifications'],
   background: {
     service_worker: 'background.js',
     type: 'module',
@@ -40,9 +39,6 @@ const manifest = {
   action: {
     default_popup: 'popup/index.html',
     default_icon: 'icon-34.png',
-  },
-  chrome_url_overrides: {
-    newtab: 'new-tab/index.html',
   },
   icons: {
     '128': 'icon-128.png',
@@ -78,9 +74,6 @@ const manifest = {
       matches: ['*://*/*'],
     },
   ],
-  side_panel: {
-    default_path: 'side-panel/index.html',
-  },
 } satisfies ManifestType;
 
 export default manifest;
